8ad1b49402a95b7328ea87c70b7a5f50582b8901
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-12-04 21:19:08 -0600

    update day 2 for part 2

--- 2022/day_2.py
+++ 2022/day_2.py
@@ -4,57 +4,38 @@
 # B = Paper
 # C = Scissors
 
-# X = Rock
-# Y = Paper
-# Z = Scissors
+# X = Rock / Lose
+# Y = Paper / Draw
+# Z = Scissors / win
 
 # Scoring
 # 1 for Rock, 2 for Paper, 3 for Scissors
 # 0 for L, 3 for D, 6 for W
 
-shape_selection_score_lookup = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3,
-}
 
-outcome_score_lookup = {
-    -1: 0,
-    0: 3,
-    1: 6,
-}
-
-
-def determine_winner(opponent, suggestion):
-    """return 1 for win, -1 for loss, and 0 for tie"""
+def get_strategy_score(opponent, suggestion):
+    """return set for outcome score in shape of [outcome score, shape score]"""
     if opponent == 'A':
         if suggestion == 'X':
-            return 0
+            return [0, 3]
         elif suggestion == 'Y':
-            return 1
+            return [3, 1]
         elif suggestion == 'Z':
-            return -1
+            return [6, 2]
     elif opponent == 'B':
         if suggestion == 'X':
-            return -1
+            return [0, 1]
         elif suggestion == 'Y':
-            return 0
+            return [3, 2]
         elif suggestion == 'Z':
-            return 1
+            return [6, 3]
     elif opponent == 'C':
         if suggestion == 'X':
-            return 1
+            return [0, 2]
         elif suggestion == 'Y':
-            return -1
+            return [3, 3]
         elif suggestion == 'Z':
-            return 0
-
-
-def calculate_score(shape, outcome):
-    """Given shape and outcome of match, return the score"""
-    shape_score = shape_selection_score_lookup[shape]
-    outcome_score = outcome_score_lookup[outcome]
-    return shape_score + outcome_score
+            return [6, 1]
 
 
 def main():
@@ -63,8 +44,8 @@ def main():
     with open('day_2_input.txt', encoding="utf8") as f:
         for line in f:
             [opponent, suggestion] = line.strip().split(' ')
-            outcome = determine_winner(opponent, suggestion)
-            total_score += calculate_score(suggestion, outcome)
+            outcome = get_strategy_score(opponent, suggestion)
+            total_score += sum(outcome)
 
     print(total_score)
 
